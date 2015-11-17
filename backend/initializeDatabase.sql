33ff14beaffc8305c3ca9be593b3fe4b7a016782
aokeson <Alexandra.Okeson@colorado.edu>
2015-11-16 20:55:37 -0700

    Update .sql files

--- backend/initializeDatabase.sql
+++ backend/initializeDatabase.sql
@@ -4,28 +4,31 @@ USE `FamishedBuffs`;
 
 CREATE TABLE IF NOT EXISTS `DiningHalls` (
   `ID` varchar(4) DEFAULT NULL,
-  `Hall` varchar(64) DEFAULT NULL,
-  `Station` varchar(64) DEFAULT NULL
+  `Hall` varchar(64) DEFAULT NULL
 );
 
 CREATE TABLE IF NOT EXISTS `Hours` (
   `ID` varchar(4) DEFAULT NULL,
   `Open` varchar(6) DEFAULT NULL,
   `Close` varchar(6) DEFAULT NULL,
-  `Description` varchar(10) DEFAULT NULL
+  `Day` varchar(10) DEFAULT NULL,
+  `Meal` varchar(15) DEFAULT NULL
 );
 
 CREATE TABLE IF NOT EXISTS `Breakfast` (
   `ID` varchar(4) DEFAULT NULL,
+  `Day` varchar(10) DEFAULT NULL,
   `Item` varchar(64) DEFAULT NULL
 );
 
 CREATE TABLE IF NOT EXISTS `Lunch` (
   `ID` varchar(4) DEFAULT NULL,
+  `Day` varchar(10) DEFAULT NULL,
   `Item` varchar(64) DEFAULT NULL
 );
 
 CREATE TABLE IF NOT EXISTS `Dinner` (
   `ID` varchar(4) DEFAULT NULL,
+  `Day` varchar(10) DEFAULT NULL,
   `Item` varchar(64) DEFAULT NULL
 );
